360db5e1df2c5bdf94429d178d3b53acef467a3f
mitch2be@gmail.com <mitch2be@gmail.com>
2014-05-13 13:53:05 -0500

    Initial Commit

---
+++ Desktop/Xcode/Adventure/Adventure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventure
+//
+//  Created by Blake Mitchell on 5/13/14.
+//  Copyright (c) 2014 Blake Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
